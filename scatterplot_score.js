c7aa0e69a1de6a9b4fd427d6d3e46e27b926f912
alittek <alina.littek@gmail.com>
2023-11-30 18:28:18 +0800

    plot layout and axis

--- scatterplot_score.js
+++ scatterplot_score.js
@@ -2,7 +2,7 @@
 const scatter_scoreDiv = document.getElementById("scatterplot_score");
 scatter_scoreDiv.style.position = "relative";
 scatter_scoreDiv.style.left = 1000 + "px"; // adjust as needed
-scatter_scoreDiv.style.top = 0 + "px"; // adjust as needed
+scatter_scoreDiv.style.top = -350 + "px"; // adjust as needed
 
 // Map data to the needs of the chart, groupby etc
 function getScatterScoreData(data) {
@@ -29,13 +29,63 @@ function updateScatterScore(data) {
 }
 
 // Do the plot here
-function plotScatterScore(data) {
+function plotScatterScore(data, scatterTotalWidth = 600, scatterTotalHeight = 400, animationDelay = 2000) {
 
-  const transformedData = getScatterScoreData(data)
+    const transformedData = getScatterScoreData(data)
 
-  // const g = SVG.append("g")
-  //   .attr("transform", `translate(${x + margin.left}, ${y + margin.top})`)
-  //.......
+    let margin = { top: 30, right: 30, bottom: 40, left: 100 },
+    scatterWidth = scatterTotalWidth - margin.left - margin.right,
+    scatterHeight = scatterTotalHeight - margin.top - margin.bottom;
+
+    let moneyData = getScatterScoreData(data);
+
+    const svg = d3.select("#scatterplot_score")
+        .append("svg")
+        .attr("width", scatterTotalWidth)
+        .attr("height", scatterTotalHeight)
+
+    const g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // add title
+    g.append("text")
+        .attr("x", (scatterWidth / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text("Audience vs Critic Score");
+    
+    // add x axis
+    let x = d3.scaleLinear()
+        .domain([0, d3.max(moneyData, d => d.imdb)])
+        .range([0, scatterWidth]);
+    g.append("g")
+        .attr("transform", "translate(0," + scatterHeight + ")")
+        .call(d3.axisBottom(x));
+
+    // add x axis label
+    g.append("text")
+        .attr("x", scatterWidth / 2)
+        .attr("y", scatterHeight + margin.bottom)
+        .attr("text-anchor", "middle")
+        .text("IMDB Score");
+
+    // add y axis
+    let y = d3.scaleLinear()
+        .domain([0, d3.max(moneyData, d => d.metascore)])
+        .range([scatterHeight, 0]);
+    g.append("g")
+        .call(d3.axisLeft(y));
+
+    // add y axis label
+    g.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (scatterHeight / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Metascore");
 
 
   //Store everything here that the update will need
