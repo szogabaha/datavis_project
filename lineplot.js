c1f55016c445b821fd5b967fb045e89b9bea2353
rjuez00 <rjuez00@gmail.com>
2023-11-30 17:12:57 +0800

    cleanded skeleton

--- lineplot.js
+++ lineplot.js
@@ -1,21 +1,23 @@
 // Map data to the needs of the chart, groupby etc
-function getLineData(data, scorekind = "rotten_tomatoes") {
+function getLineData(data) {
 
   // get only those that have the field "selected" to true
   data = data.filter(d => d.selected);
 
+  console.log(data);
+
 }
 
 function updateLine(data) {
-  const transformedData = getBarData(data);
+  const transformedData = getLineData(data);
   //TODO
 }
 
-function plotLine(data, barTotalWidth = 1000, barTotalHeight = 400, animationDelay = 2000) {
+function plotLine(data, lineTotalWidth = 1000, lineTotalHeight = 400, animationDelay = 2000) {
   
-  let barMargin = { top: 30, right: 30, bottom: 40, left: 100 },
-  barWidth = barTotalWidth - barMargin.left - barMargin.right,
-  barHeight = barTotalHeight - barMargin.top - barMargin.bottom;
+  let lineMargin = { top: 30, right: 30, bottom: 40, left: 100 },
+  lineWidth = lineTotalWidth - lineMargin.left - lineMargin.right,
+  lineHeight = lineTotalHeight - lineMargin.top - lineMargin.bottom;
 
   let processedData = getLineData(data);
 
