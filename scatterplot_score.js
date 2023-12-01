ef10d9e675df404e6762153dde626a10fc6da598
alittek <alina.littek@gmail.com>
2023-12-01 17:21:33 +0800

    smooth update function

--- scatterplot_score.js
+++ scatterplot_score.js
@@ -1,3 +1,7 @@
+let scatterTotalWidth = 600, scatterTotalHeight = 400;
+let margin = { top: 30, right: 30, bottom: 40, left: 100 },
+    scatterWidth = scatterTotalWidth - margin.left - margin.right,
+    scatterHeight = scatterTotalHeight - margin.top - margin.bottom;
 
 // Map data to the needs of the chart, groupby etc
 function getScatterScoreData(data) {
@@ -18,25 +22,52 @@ function getScatterScoreData(data) {
     return data;
 }
 
-
 // Obj, all data that the updateEx1Chart function needs
 var EX1_CONFIG = null;
 
 // Update the chart according to some request
 // Receives the filtered data!
 function updateScatterScore(data) {
-  const transformedData = getScatterScoreData(data);
-  //Do the transition here using EX1_CONFIG + transformedData
+    const transformedData = getScatterScoreData(data);
+    //Do the transition here using EX1_CONFIG + transformedData
+
+    // Define scales based on the new data
+    let x = d3.scaleLinear()
+        .domain([d3.min(transformedData, d => d.imdb), d3.max(transformedData, d => d.imdb)])
+        .range([0, scatterWidth]);
+    let y = d3.scaleLinear()
+        .domain([d3.min(transformedData, d => d.metascore), d3.max(transformedData, d => d.metascore)])
+        .range([scatterHeight, 0]);
+    var color = d3.scaleLinear()
+        .domain([d3.min(transformedData, d => d.revenue), 0, (d3.max(transformedData, d => d.revenue))/8, (d3.max(transformedData, d => d.revenue))/4 , d3.max(transformedData, d => d.revenue)])
+        .range(["red", "orange", "yellow", "green", "blue"]);
+
+    // Select the circles and bind the new data
+    let circles = d3.select("#scatterplot_score").selectAll("circle").data(transformedData);
+
+    // Use the general update pattern to handle the enter, update, and exit selections
+    circles.enter().append("circle")
+        .attr("r", 5)
+        .attr("opacity", 0.7)
+        .merge(circles) // Merges the enter and update selections
+        .transition() // Initiates a transition
+        .duration(1000) // Specifies the duration
+        .attr("cx", d => x(d.imdb))
+        .attr("cy", d => y(d.metascore))
+        .attr("fill", d => color(d.revenue));
+
+    // Transition the exiting circles to have zero radius and then remove them
+    circles.exit()
+        .transition()
+        .duration(800)
+        .attr("r", 0)
+        .remove();
 }
 
 // Do the plot here
 function plotScatterScore(data, scatterTotalWidth = 600, scatterTotalHeight = 400, animationDelay = 2000) {
 
     const transformedData = getScatterScoreData(data)
-
-    let margin = { top: 30, right: 30, bottom: 40, left: 100 },
-    scatterWidth = scatterTotalWidth - margin.left - margin.right,
-    scatterHeight = scatterTotalHeight - margin.top - margin.bottom;
 
     let scoreData = getScatterScoreData(data);
 
