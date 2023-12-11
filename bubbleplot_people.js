1b385f2b3798498398d5963172f16d3004882208
szogabaha <szolnokgabcsi@gmail.com>
2023-12-11 21:22:05 +0800

    format files

--- bubbleplot_people.js
+++ bubbleplot_people.js
@@ -1,236 +1,252 @@
+// TODO put this into the BUBBLE_CONFIG
 FONT_SIZE = 14;
-
-//EX: 22500000 => 22.5M
+// global config for plot
+var BUBBLE_CONFIG = null;
+// EX: 22500000 => 22.5M, for tooltip
 function formatMillions(revenue) {
-  if (typeof revenue !== 'number') {
-    return 'Invalid input';
-  }
+	if (typeof revenue !== 'number') {
+		return 'Invalid input';
+	}
 
-  if (Math.abs(revenue) >= 1e6) {
-    const formattedRevenue = (revenue / 1e6).toFixed(1);
-    return `${formattedRevenue}M`;
-  }
+	if (Math.abs(revenue) >= 1e6) {
+		const formattedRevenue = (revenue / 1e6).toFixed(1);
+		return `${formattedRevenue}M`;
+	}
 
-  return revenue.toString();
+	return revenue.toString();
 };
 
 
+// parse string representation to array
 function getPeopleFromObject(str) {
-  try {
-    const array = JSON.parse(str.replace(/'/g, '"'));
-    return array;
-  }
-  catch (error) { //It's not an array, parse it as a string
-    return [str == "" ? "Unknown" : str]
-  }
+	try {
+		const array = JSON.parse(str.replace(/'/g, '"'));
+		return array;
+	}
+	catch (error) { //It's not an array, parse it as a string
+		return [str == "" ? "Unknown" : str]
+	}
 }
 
-function mapPerson(e, acc, obj, position) {
-  {
-    const existingObject = acc.find(accItem => accItem.name === e && accItem.position === position);
-    if (existingObject) {
-      existingObject.count++;
-      existingObject.movies.push(obj.title)
-      if (obj.selected) {
-        existingObject.selectedCount++;
-        existingObject.revenue += obj.revenue;
-      }
-    } else {
-      const newObj = {
-        name: e, count: 1,
-        position: position,
-        movies: [obj.title],
-        revenue: obj.selected ? obj.revenue : 0.1,
-        selectedCount: obj.selected ? 1 : 0
-      };
-      acc.push(newObj);
-    }
-  }
+// use the given person to accumulate the received acc.
+// Identifier of an object: name and position (since a name can be associated
+// with more positions but we want 1-1 assoc)
+function accumulatePerson(e, acc, obj, position) {
+	{
+		const existingObject = acc.find(accItem => accItem.name === e && accItem.position === position);
+		if (existingObject) {
+			existingObject.count++;
+			existingObject.movies.push(obj.title)
+			if (obj.selected) {
+				existingObject.selectedCount++;
+				// Only increase revenue if obj is selected.
+				// Implicit filter
+				existingObject.revenue += obj.revenue;
+			}
+		} else {
+			const newObj = {
+				name: e, count: 1,
+				position: position,
+				movies: [obj.title],
+				revenue: obj.selected ? obj.revenue : 0.1,
+				selectedCount: obj.selected ? 1 : 0
+			};
+			acc.push(newObj);
+		}
+	}
 }
 
 
-// Split the list by position, crop top N objects in each list
+// Split the list by position, keep the revenue of the top N objects in each list
 // and merge the result (result: a merged list of
-/// top N director, top N editor, top N musician)
+// top N director, top N editor, top N musician, rest wont be represented)
 function getTopRevenueObjects(data, N) {
-  const topRevenueByPosition = data.reduce((result, item) => {
-    const { position, revenue } = item;
+	const topRevenueByPosition = data.reduce((result, item) => {
+		const { position, revenue } = item;
 
-    // Initialize an array for the position if not exists
-    result[position] = result[position] || [];
+		// Initialize an array for the position if not exists
+		result[position] = result[position] || [];
 
-    // Add the current item to the array for the position
-    result[position].push(item);
+		// Add the current item to the array for the position
+		result[position].push(item);
 
-    // Sort the array for the position by revenue in descending order
-    result[position].sort((a, b) => b.revenue - a.revenue);
+		// Sort the array for the position by revenue in descending order
+		result[position].sort((a, b) => b.revenue - a.revenue);
 
-    // Keep only the top N items for each position
-    result[position].forEach((innerItem, idx) => {
-      if(idx >= N) {
-        result[position][idx].revenue = 0;
-      }
-    });
-    return result;
-  }, {});
+		// Keep only the top N items for each position
+		// Implicit filter
+		result[position].forEach((innerItem, idx) => {
+			if (idx >= N) {
+				result[position][idx].revenue = 0;
+			}
+		});
+		return result;
+	}, {});
 
-  const mergedArray = Object.values(topRevenueByPosition).flat();
-  return mergedArray;
+	const mergedArray = Object.values(topRevenueByPosition).flat();
+	return mergedArray;
 }
 
 // Map data to the needs of the chart, groupby etc
 function getBubblePeopleData(data) {
-  let unsorted = data.reduce((acc, obj) => {
-    const directors = getPeopleFromObject(obj["Directed by"]);
-    directors.forEach(e => mapPerson(e, acc, obj, "director"));
-    const editors = getPeopleFromObject(obj["Edited by"]);
-    editors.forEach(e => mapPerson(e, acc, obj, "editor"));
-    const musicians = getPeopleFromObject(obj["Music by"]);
-    musicians.forEach(e => mapPerson(e, acc, obj, "musician"));
+	let unsorted = data.reduce((acc, obj) => {
+		const directors = getPeopleFromObject(obj["Directed by"]);
+		directors.forEach(e => accumulatePerson(e, acc, obj, "director"));
+		const editors = getPeopleFromObject(obj["Edited by"]);
+		editors.forEach(e => accumulatePerson(e, acc, obj, "editor"));
+		const musicians = getPeopleFromObject(obj["Music by"]);
+		musicians.forEach(e => accumulatePerson(e, acc, obj, "musician"));
 
-    return acc;
-  }, []);
+		return acc;
+	}, []);
 
-  unsorted = getTopRevenueObjects(unsorted, 10);
-  let sorted = unsorted.sort((a, b) => b.name - a.name);
-  return sorted;
+	// Crop top N
+	unsorted = getTopRevenueObjects(unsorted, 10);
+	let sorted = unsorted.sort((a, b) => b.name - a.name);
+	return sorted;
 }
-
-
-// Obj, all data that the updateEx1Chart function needs
-var BUBBLE_CONFIG = null;
 
 // Update the chart according to some request
 // Receives the filtered data!
 function updateBubblePeople(data) {
-  const transformedData = getBubblePeopleData(data);
+	// Generate new data
+	const transformedData = getBubblePeopleData(data);
+	const root = BUBBLE_CONFIG.pack(d3.hierarchy({ children: transformedData })
+		.sum(d => d.revenue));
 
-  const root = BUBBLE_CONFIG.pack(d3.hierarchy({ children: transformedData })
-    .sum(d => d.revenue));
-  const bubbles = BUBBLE_CONFIG.bubbles.data(root.leaves());
-  
+	// Update plot with new data 
+	BUBBLE_CONFIG.bubbles.data(root.leaves());
 
-  BUBBLE_CONFIG.bubbles
-    .transition()
-    .duration(1000)
-    .attr("r", d => d.r)
-    .attr("stroke", "black")
-    .attr("stroke-width", d => `${BUBBLE_CONFIG.selectedPeople.map(x => x.name).includes(d.data.name)? 5 : 0}`)
-    .attr("transform", d => `translate(${d.x},${d.y})`);
+	BUBBLE_CONFIG.bubbles
+		.transition()
+		.duration(1000)
+		.attr("r", d => d.r)
+		.attr("stroke", "black")
+		.attr("stroke-width", d => `${BUBBLE_CONFIG.selectedPeople.map(x => x.name).includes(d.data.name) ? 5 : 0}`)
+		.attr("transform", d => `translate(${d.x},${d.y})`);
 
-  
+
 }
 
 // Do the plot here
 function plotBubblePeople(data, width = 600, height = 500) {
 
-  let selectedPeople = [];
-  // Set margins and dimensions for the chart
-  const margin = { top: 45, right: 50, bottom: 0, left: 20 };
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
+	let selectedPeople = [];
+	// Set margins and dimensions for the chart
+	const margin = { top: 45, right: 50, bottom: 0, left: 20 };
+	const innerWidth = width - margin.left - margin.right;
+	const innerHeight = height - margin.top - margin.bottom;
 
-  const transformedData = getBubblePeopleData(data);
-  // Set up the SVG container
-  const svg = d3.select('#bubbleplot') // Select the body element or use an existing container
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
-
-
-  // Title
-  svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", margin.top - 20)
-    .attr("text-anchor", "middle")
-    .attr("font-size", 24)
-    .style("text-decoration", "underline")
-    .text("People vs Revenue");
-
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
-  const pack = d3.pack()
-    .size([innerWidth, innerHeight])
-    .padding(3);
-
-  const root = pack(d3.hierarchy({ children: transformedData })
-    .sum(d => d.revenue));
-
-  const node = svg.append("g")
-    .attr("transform", d => `translate(${margin.left},${margin.top})`)
-    .selectAll("circle")
-    .data(root.leaves())
-    .enter()
-    .append("circle")
-    .attr("fill", d => color(d.data.position))
-    .attr("r", d => d.r)
-    .attr("transform", d => `translate(${d.x},${d.y})`)
-    .on('click', d => {
-      if(BUBBLE_CONFIG.selectedPeople.filter(x => x.name == d.data.name && x.position == d.data.position).length === 0) {
-        BUBBLE_CONFIG.selectedPeople.push(d.data);
-      } else {
-        BUBBLE_CONFIG.selectedPeople = BUBBLE_CONFIG.selectedPeople.filter(x => !(x.name == d.data.name && x.position == d.data.position));
-      }
-      
-      data.forEach(e => e.bubbleSelected = BUBBLE_CONFIG.selectedPeople.length == 0 ||
-                               BUBBLE_CONFIG.selectedPeople.filter(sP => sP.movies.includes(e.title)).length != 0);
-      
-      updateCharts(data);
-    })
-    .on("mouseover", (d, i) => {
-      tooltip.transition()
-        .duration(200)
-        .style("opacity", .9);
-      tooltip.html(d.data.name + "<br/>" + "Revenue: " + formatMillions(d.data.revenue))
-        .style("left", (d3.event.pageX) + "px")
-        .style("top", (d3.event.pageY - 28) + "px");
-    })
-    .on("mouseout", d => {
-      tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
-    });
+	const transformedData = getBubblePeopleData(data);
+	// Set up the SVG container
+	const svg = d3.select('#bubbleplot') // Select the body element or use an existing container
+		.append('svg')
+		.attr('width', width)
+		.attr('height', height);
 
 
-  // add legend
-  const legend = svg.append('g')
-    .attr("class", "legend")
-    .attr("transform", (d, i) => `translate(${innerWidth - 50},${margin.top})`);
+	// Title
+	svg.append("text")
+		.attr("x", (width / 2))
+		.attr("y", margin.top - 20)
+		.attr("text-anchor", "middle")
+		.attr("font-size", 24)
+		.style("text-decoration", "underline")
+		.text("People vs Revenue");
+
+	const color = d3.scaleOrdinal(d3.schemeCategory10);
+	const pack = d3.pack()
+		.size([innerWidth, innerHeight])
+		.padding(3);
+
+	const root = pack(d3.hierarchy({ children: transformedData })
+		.sum(d => d.revenue));
+
+	const node = svg.append("g")
+		.attr("transform", d => `translate(${margin.left},${margin.top})`)
+		.selectAll("circle")
+		.data(root.leaves())
+		.enter()
+		.append("circle")
+		.attr("fill", d => color(d.data.position))
+		.attr("r", d => d.r)
+		.attr("transform", d => `translate(${d.x},${d.y})`)
+		.on('click', d => {
+
+			// Object comparator
+			isSelected = (x) => x.name == d.data.name && x.position == d.data.position;
+
+			// if the clicked person is not added to the filters, add it, otherwise remove it
+			if (BUBBLE_CONFIG.selectedPeople.filter(isSelected).length === 0) {
+				BUBBLE_CONFIG.selectedPeople.push(d.data);
+			} else {
+				BUBBLE_CONFIG.selectedPeople = BUBBLE_CONFIG.selectedPeople.filter(x => !(isSelected(x)));
+			}
+
+			// Modify model according to the set filters
+			data.forEach(e => e.bubbleSelected = BUBBLE_CONFIG.selectedPeople.length == 0 ||
+				BUBBLE_CONFIG.selectedPeople.filter(sP => sP.movies.includes(e.title)).length != 0);
+
+			// Change view
+			updateCharts(data);
+		})
+		// Rest is tooltip
+		.on("mouseover", (d, i) => {
+			tooltip.transition()
+				.duration(200)
+				.style("opacity", .9);
+			tooltip.html(d.data.name + "<br/>" + "Revenue: " + formatMillions(d.data.revenue))
+				.style("left", (d3.event.pageX) + "px")
+				.style("top", (d3.event.pageY - 28) + "px");
+		})
+		.on("mouseout", d => {
+			tooltip.transition()
+				.duration(500)
+				.style("opacity", 0);
+		});
 
 
-  const legendData = [...new Set(transformedData.map(d => d.position))];
+	// add legend
+	const legend = svg.append('g')
+		.attr("class", "legend")
+		.attr("transform", (d, i) => `translate(${innerWidth - 50},${margin.top})`);
 
-  legend.append('g')
-    .selectAll("rect")
-    .data(legendData)
-    .enter()
-    .append('rect')
-    .attr('y', (d, i) => (FONT_SIZE * i * 1.8))
-    .attr('width', FONT_SIZE)
-    .attr('height', FONT_SIZE)
-    .attr('fill', (d, i) => color(d))
-    .attr('stroke', 'black')
-    .style('stroke-width', '1px')
-    .on('click', d => { });
+	// Create legend data from the original dataset 
+	// (discrete position values)
+	const legendData = [...new Set(transformedData.map(d => d.position))];
 
-  legend.append('g')
-    .selectAll('text')
-    .data(legendData)
-    .enter()
-    .append('text')
-    .attr("cursor", "default")
-    .text(d => d)
-    .attr('x', (d, i) => FONT_SIZE * 1.8)
-    .attr('y', (d, i) => FONT_SIZE - 1 + 1.8 * FONT_SIZE * i)
-    .style('font-size', `${FONT_SIZE}`)
-    .on('click', d => { });
+	legend.append('g')
+		.selectAll("rect")
+		.data(legendData)
+		.enter()
+		.append('rect')
+		.attr('y', (d, i) => (FONT_SIZE * i * 1.8))
+		.attr('width', FONT_SIZE)
+		.attr('height', FONT_SIZE)
+		.attr('fill', (d, i) => color(d))
+		.attr('stroke', 'black')
+		.style('stroke-width', '1px')
+		.on('click', d => { });
 
-  BUBBLE_CONFIG = {
-    "color": color,
-    "bubbles": node,
-    "root": root,
-    "pack": pack,
-    "selectedPeople": selectedPeople
-  }
+	legend.append('g')
+		.selectAll('text')
+		.data(legendData)
+		.enter()
+		.append('text')
+		.attr("cursor", "default")
+		.text(d => d)
+		.attr('x', (d, i) => FONT_SIZE * 1.8)
+		.attr('y', (d, i) => FONT_SIZE - 1 + 1.8 * FONT_SIZE * i)
+		.style('font-size', `${FONT_SIZE}`)
+		.on('click', d => { });
+
+	BUBBLE_CONFIG = {
+		"color": color,
+		"bubbles": node,
+		"root": root,
+		"pack": pack,
+		"selectedPeople": selectedPeople
+	}
 }
 
 
--- pieplot_countries.js
+++ pieplot_countries.js
@@ -1,60 +1,75 @@
-
+// TODO add these to global repo
 FONT_SIZE = 14;
 TRANSITION_TIME = 2000;
+
+// Global variables collected, related to this chart
 var PIECOUNTRIES_CONFIG = null;
 
-function clickOnLegend(event, data) {
-    if(!PIECOUNTRIES_CONFIG.deselectedCountries.includes(event.country)) {
-        PIECOUNTRIES_CONFIG.deselectedCountries.push(event.country);
-    
-    } else {
-        PIECOUNTRIES_CONFIG.deselectedCountries = PIECOUNTRIES_CONFIG.deselectedCountries.filter(x => x !== event.country);
-    }
 
-    data.map(e => {
-        const remainingCountries = getCountryFromObject(e).filter(remaining => !PIECOUNTRIES_CONFIG.deselectedCountries.includes(remaining))
-        e["pieSelected"] = remainingCountries.length !== 0;
-    });
+// Click on pie slice or legend part, to deselect countries
+// Update deselect list and the model, then call the controller.
+// If the clicked country is not deselected, deselect it, otherwise remove from filters
+function clickOnItem(event, data) {
 
-    updateCharts(data);
+	if (!PIECOUNTRIES_CONFIG.deselectedCountries.includes(event.country)) {
+		PIECOUNTRIES_CONFIG.deselectedCountries.push(event.country);
+
+	} else {
+		PIECOUNTRIES_CONFIG.deselectedCountries = PIECOUNTRIES_CONFIG.deselectedCountries.filter(x => x !== event.country);
+	}
+
+	data.map(e => {
+		const remainingCountries = getCountryFromObject(e).filter(remaining => !PIECOUNTRIES_CONFIG.deselectedCountries.includes(remaining))
+		e["pieSelected"] = remainingCountries.length !== 0;
+	});
+
+	updateCharts(data);
 
 }
 
+// Parse string to list of countries
 function getCountryFromObject(obj) {
-    try {
-        const countryArray = JSON.parse(obj.Country.replace(/'/g, '"'));
-        return countryArray;
-    }
-    catch (error) { //Country field is not an array, parse it as a string
-        return [obj.Country == "" ? "Unknown" : obj.Country]
-    }
+	try {
+		const countryArray = JSON.parse(obj.Country.replace(/'/g, '"'));
+		return countryArray;
+	}
+	catch (error) { //Country field is not an array, parse it as a string
+		return [obj.Country == "" ? "Unknown" : obj.Country]
+	}
 }
 
+// Input: raw data
+// Output: [ Obj {country, count, selectedCount}]
+// selectedCount is cropped to 0 if the country is deselected in this top
+// (to avoid having a country in the plot if we deselected it because of another movie)
 function getPieCountriesData(data) {
 
-    //////////////////
-    // Use reduce to transform the array
-    unsorted = data.reduce((acc, obj) => {
-        const countries = getCountryFromObject(obj);
-        countries.forEach(e => {
-            const existingObject = acc.find(accItem => accItem.country === e);
-            const showInThisPlot = obj.selected && !PIECOUNTRIES_CONFIG?.deselectedCountries.includes(e);
-            if (existingObject) {
-                existingObject.count++;
-                if (showInThisPlot) {
-                    existingObject.selectedCount++;
-                }
-            } else {
-                const newObj = { country: e, count: 1, selectedCount: showInThisPlot ? 1 : 0 };
-                acc.push(newObj);
-            }
-        });
+	unsorted = data.reduce((acc, obj) => {
 
-        return acc;
-    }, []);
 
-    sorted = unsorted.sort((a, b) => b.count - a.count);
-    return sorted;
+		const countries = getCountryFromObject(obj);
+		countries.forEach(e => {
+			const existingObject = acc.find(accItem => accItem.country === e);
+
+			// we only show countries that are not filtered out by other plots (obj.selected) and by this
+			// (second part of the expression)
+			const showInThisPlot = obj.selected && !PIECOUNTRIES_CONFIG?.deselectedCountries.includes(e);
+			if (existingObject) {
+				existingObject.count++;
+				if (showInThisPlot) {
+					existingObject.selectedCount++;
+				}
+			} else {
+				const newObj = { country: e, count: 1, selectedCount: showInThisPlot ? 1 : 0 };
+				acc.push(newObj);
+			}
+		});
+
+		return acc;
+	}, []);
+
+	sorted = unsorted.sort((a, b) => b.count - a.count);
+	return sorted;
 }
 
 
@@ -63,138 +78,143 @@ function getPieCountriesData(data) {
 // Update the chart according to some request
 // Receives the filtered data!
 function updatePieCountries(data) {
-    updatedData = getPieCountriesData(data);
-    let oldAngles = PIECOUNTRIES_CONFIG["sections"].data();
+	updatedData = getPieCountriesData(data);
+	let oldAngles = PIECOUNTRIES_CONFIG["sections"].data();
 
-    PIECOUNTRIES_CONFIG["sections"]
-        .data(PIECOUNTRIES_CONFIG["pie"](updatedData.map(x => x.selectedCount)))
-        .transition()
-        .duration(TRANSITION_TIME)
-        .attrTween('d', function (d) {
-            let starti = d3.interpolate(oldAngles[d.index].startAngle, d.startAngle);
-            let endi = d3.interpolate(oldAngles[d.index].endAngle, d.endAngle);
-            return function (t) {
-                d.startAngle = starti(t);
-                d.endAngle = endi(t);
-                return PIECOUNTRIES_CONFIG["arcGenerator"](d);
-            }
-        });
+	PIECOUNTRIES_CONFIG["sections"]
+		.data(PIECOUNTRIES_CONFIG["pie"](updatedData.map(x => x.selectedCount)))
+		.transition()
+		.duration(TRANSITION_TIME)
+		.attrTween('d', function (d) {
+			let starti = d3.interpolate(oldAngles[d.index].startAngle, d.startAngle);
+			let endi = d3.interpolate(oldAngles[d.index].endAngle, d.endAngle);
+			return function (t) {
+				d.startAngle = starti(t);
+				d.endAngle = endi(t);
+				return PIECOUNTRIES_CONFIG["arcGenerator"](d);
+			}
+		});
 
-    PIECOUNTRIES_CONFIG["legend"].selectAll("text")
-                                .style('fill', "black") //Reset to black
-                                .filter((d, i) => PIECOUNTRIES_CONFIG.deselectedCountries.includes(d.country))
-                                .style('fill', "grey"); //Set deselected to grey
+	// set deselected country legends to grey
+	PIECOUNTRIES_CONFIG["legend"].selectAll("text")
+		.style('fill', "black") // Reset to black
+		.filter((d, i) => PIECOUNTRIES_CONFIG.deselectedCountries.includes(d.country))
+		.style('fill', "grey"); // Set deselected to grey
 
-    }
+}
 
 // Do the plot here
 function plotPieCountries(data, width = 400, height = 400) {
-    var deselectedCountries = [];
+	var deselectedCountries = [];
 
-    // Set margins and dimensions for the chart
-    const margin = { top: 45, right: 150, bottom: 0, left: 20 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-
-
-    // Set up the SVG container
-    const svg = d3.select('#pieplot') // Select the body element or use an existing container
-        .append('svg')
-        .attr('width', width + 155)
-        .attr('height', height);
-
-    const radius = Math.min(innerWidth, innerHeight) / 2;
-    const transformedData = getPieCountriesData(data);
-
-    const g = svg.append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`)
+	// Set margins and dimensions for the chart
+	const margin = { top: 45, right: 150, bottom: 0, left: 20 };
+	const innerWidth = width - margin.left - margin.right;
+	const innerHeight = height - margin.top - margin.bottom;
 
 
-    // Title
-    //addLabel(g,"Education", innerWidth / 2, 0, false);
-    svg.append("text")
-    .attr("x", 50 + (width / 2))
-    .attr("y", margin.top)
-    .attr("text-anchor", "middle")
-    .attr("font-size", 24)
-    .style("text-decoration", "underline")
-    .text("Countries");
+	// Set up the SVG container
+	const svg = d3.select('#pieplot') // Select the body element or use an existing container
+		.append('svg')
+		.attr('width', width + 155)
+		.attr('height', height);
 
-    let pie = d3.pie().sort((a, b) => (b.count - a.count));
+	const radius = Math.min(innerWidth, innerHeight) / 2;
+	const transformedData = getPieCountriesData(data);
 
-    var arcGenerator = d3.arc()
-        .innerRadius(0.001)
-        .outerRadius(radius);
-
-    // There is no 20 colored discrete colormap prepared
-    let color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', 
-                                 '#e41a1c', '#dede00', '#7a7a7a', '#ffae42', 
-                                 '#5e3c99', '#2ca02c', '#d62728', '#9467bd',
-                                 '#8c564b', '#1f77b4', '#bcbd22', '#17becf',
-                                 '#FF69B4', '#00FA9A', '#FFD700']);
-
-    let path = g.selectAll('path')
-        .data(pie(transformedData.map(x => x.count)))
-        .enter()
-        .append('path')
-        .attr("transform", `translate(${innerWidth / 2}, ${radius + margin.top})`)
-        .attr('d', arcGenerator)
-        .attr('fill', (d, i) => color(i))
-        .on('click', (d, i) => clickOnLegend(transformedData[i], data))
-        .on("mouseover", (d,i) => {
-            tooltip.transition()
-              .duration(400)
-              .style("opacity", .9);
-            tooltip.html("Country: " + transformedData[i].country + "<br/>" + "Num. of movies: " + transformedData[i].selectedCount)
-              .style("left", (d3.event.pageX) + "px")
-              .style("top", (d3.event.pageY - 28) + "px");
-          })
-          .on("mouseout", d => {
-            tooltip.transition()
-              .duration(500)
-              .style("opacity", 0);
-          });
-
-    //Legend
-    const legend = g
-        .append('g')
-        .attr('transform', `translate(${innerWidth / 2 + radius + 15}, ${radius})`);
-
-    legend.append('g')
-        .selectAll("rect")
-        .data(transformedData)
-        .enter()
-        .append('rect')
-        .attr('id', d => d.country)
-        .attr('x', (d, i) => i < 9 ? 0 : FONT_SIZE * 10)
-        .attr('y', (d, i) => (FONT_SIZE * (i % 9) * 1.8))
-        .attr('width', FONT_SIZE)
-        .attr('height', FONT_SIZE)
-        .attr('fill', (d, i) => color(i))
-        .attr('stroke', 'black')
-        .style('stroke-width', '1px')
-        .on('click', d => clickOnLegend(d, data));
-
-    legend
-        .selectAll('text')
-        .data(transformedData)
-        .enter()
-        .append('text')
-        .attr('id', d => d.country)
-        .attr("cursor", "default")
-        .text(d => d.country)
-        .attr('x',(d,i) => i < 9 ? FONT_SIZE * 1.4 : FONT_SIZE * 11.4)
-        .attr('y', (d, i) => FONT_SIZE * (i % 9) * 1.8 + FONT_SIZE - 2)
-        .style('font-size', `${FONT_SIZE}px`)
-        .on('click', d => clickOnLegend(d, data));
+	const g = svg.append("g")
+		.attr("transform", `translate(${margin.left}, ${margin.top})`)
 
 
-    PIECOUNTRIES_CONFIG = {
-        "pie": pie,
-        "arcGenerator": arcGenerator,
-        "sections": path,
-        "legend": legend,
-        "deselectedCountries": deselectedCountries,
-        "color": color
-    }
+	// Title
+	svg.append("text")
+		.attr("x", 50 + (width / 2))
+		.attr("y", margin.top)
+		.attr("text-anchor", "middle")
+		.attr("font-size", 24)
+		.style("text-decoration", "underline")
+		.text("Countries");
+
+	let pie = d3.pie().sort((a, b) => (b.count - a.count));
+
+	var arcGenerator = d3.arc()
+		.innerRadius(0.001)
+		.outerRadius(radius);
+
+	// There is no 20 colored discrete colormap prepared
+	let color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3',
+		'#e41a1c', '#dede00', '#7a7a7a', '#ffae42',
+		'#5e3c99', '#2ca02c', '#d62728', '#9467bd',
+		'#8c564b', '#1f77b4', '#bcbd22', '#17becf',
+		'#FF69B4', '#00FA9A', '#FFD700']);
+
+
+	let path = g.selectAll('path')
+		.data(pie(transformedData.map(x => x.count)))
+		.enter()
+		.append('path')
+		.attr("transform", `translate(${innerWidth / 2}, ${radius + margin.top})`)
+		.attr('d', arcGenerator)
+		.attr('fill', (d, i) => color(i))
+		.on('click', (d, i) => clickOnItem(transformedData[i], data))
+		// Tooltip from here
+		.on("mouseover", (d, i) => {
+			tooltip.transition()
+				.duration(400)
+				.style("opacity", .9);
+			tooltip.html("Country: " + transformedData[i].country + "<br/>" + "Num. of movies: " + transformedData[i].selectedCount)
+				.style("left", (d3.event.pageX) + "px")
+				.style("top", (d3.event.pageY - 28) + "px");
+		})
+		.on("mouseout", d => {
+			tooltip.transition()
+				.duration(500)
+				.style("opacity", 0);
+		});
+
+	// Legend
+	const legend = g
+		.append('g')
+		.attr('transform', `translate(${innerWidth / 2 + radius + 15}, ${radius})`);
+
+	// Legend colorboxes
+	legend.append('g')
+		.selectAll("rect")
+		.data(transformedData)
+		.enter()
+		.append('rect')
+		.attr('id', d => d.country)
+		.attr('x', (d, i) => i < 9 ? 0 : FONT_SIZE * 10) //Two column
+		.attr('y', (d, i) => (FONT_SIZE * (i % 9) * 1.8))
+		.attr('width', FONT_SIZE)
+		.attr('height', FONT_SIZE)
+		.attr('fill', (d, i) => color(i))
+		.attr('stroke', 'black')
+		.style('stroke-width', '1px')
+		.on('click', d => clickOnItem(d, data));
+
+	// Legend labels
+	legend
+		.selectAll('text')
+		.data(transformedData)
+		.enter()
+		.append('text')
+		.attr('id', d => d.country)
+		.attr("cursor", "default")
+		.text(d => d.country)
+		// Two columns. different from colorbox settings since the text is written from bottom left
+		.attr('x', (d, i) => i < 9 ? FONT_SIZE * 1.4 : FONT_SIZE * 11.4)
+		.attr('y', (d, i) => FONT_SIZE * (i % 9) * 1.8 + FONT_SIZE - 2)
+		.style('font-size', `${FONT_SIZE}px`)
+		.on('click', d => clickOnItem(d, data));
+
+
+	PIECOUNTRIES_CONFIG = {
+		"pie": pie,
+		"arcGenerator": arcGenerator,
+		"sections": path,
+		"legend": legend,
+		"deselectedCountries": deselectedCountries,
+		"color": color
+	}
 }
